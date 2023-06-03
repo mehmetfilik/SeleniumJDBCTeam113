192795363d4fbed262d9588fed7bdedc9239cd91
mehmetfilik <filikmehmet02gmail.com>
2023-06-03 17:48:26 +0200

    230603

---
+++ src/test/java/JDBC_Odev_1.java
@@ -0,0 +1,12 @@
+public class JDBC_Odev_1 {
+    public static void main(String[] args) {
+
+        /*
+            Ödev
+1- Roles tablosundan name bilgisi 'Receptionist' olan rolün is_active, is_system, is_superadmin ve created_at bilgilerini alt alta yazdiriniz.
+2- Patients tablosundaki ismi 'a' ile baslayan hastalarin patient_name, age, mobileno, email bilgilerini listeleyiniz.
+3- visitors_book tablosundan purpose bilgisi 'Inguiry' olan ziyaretcilerin isimlerini ters sirali listeleyiniz.
+         */
+
+    }
+}
